c76e29151c51be95e02cbbccf2c51fa02d8eada9
chrisjbillington <chrisjbillington@gmail.com>
2020-12-27 15:18:58 +1100

    Changes to get things working on Arch

--- setup.py
+++ setup.py
@@ -25,12 +25,13 @@ def checkrun(cmd):
 
 def post_install():
     # Add udev group, add user to the group, and add udev rule to allow the
-    # group to write to /dev/uinput
+    # group to write to /dev/uinput, ensure uinput module is loaded
     try:
         checkrun(f'getent group uinput > /dev/null || groupadd uinput')
-        checkrun(f'adduser $SUDO_USER uinput')
-        checkrun(f'adduser $SUDO_USER input')
+        checkrun(f'usermod -a -G uinput $SUDO_USER')
+        checkrun(f'usermod -a -G input $SUDO_USER')
         checkrun(f'udevadm control --reload-rules && udevadm trigger')
+        checkrun(f'modprobe uinput')
     except SystemExit:
         msg = "Adding uinput group failed, did you run with sudo?\n"
         sys.stderr.write(msg)
@@ -41,20 +42,21 @@ class Install(install):
         install.run(self)
         post_install()
 
-setup(name='DElauncher4Kodi',
-      version=VERSION,
-      description="Kodi launcher grabbing media keys and adjusting system volume.",
-      author='Chris Billington',
-      author_email='chrisjbillington@gmail.com',
-      url='https://github.com/chrisjbillington/DElauncher4Kodi/',
-      license="GPL2",
-      packages=['DElauncher4Kodi'],
-      data_files = [
-                    ('share/applications', ['data/org.DElauncher4Kodi.desktop']),
-                    ('/etc/udev/rules.d',
-                         ['data/99-DElauncher4Kodi-uinput-group-access.rules'])
-                   ],
-      install_requires=['evdev', 'pulsectl'],
-      cmdclass={'install': Install},
-     )
+setup(
+    name='DElauncher4Kodi',
+    version=VERSION,
+    description="Kodi launcher grabbing media keys and adjusting system volume.",
+    author='Chris Billington',
+    author_email='chrisjbillington@gmail.com',
+    url='https://github.com/chrisjbillington/DElauncher4Kodi/',
+    license="GPL2",
+    packages=['DElauncher4Kodi'],
+    data_files=[
+        ('share/applications', ['data/org.DElauncher4Kodi.desktop']),
+        ('/etc/udev/rules.d', ['data/99-DElauncher4Kodi-uinput-group-access.rules']),
+        ('/etc/modules-load.d', ['data/DElauncher4Kodi.conf']),
+    ],
+    install_requires=['evdev', 'pulsectl'],
+    cmdclass={'install': Install},
+)
 
